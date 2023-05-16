f9efd14989b9e7bebc1011fdb3749acc97c4f9b8
Parshotan Seenanan <parshsee@gmail.com>
2023-05-15 23:21:48 -0400

    added notes for various response methods

---
+++ commands/echo.js
@@ -0,0 +1,14 @@
+import { SlashCommandBuilder } from 'discord.js';
+
+const exportedMethods = {
+	data: new SlashCommandBuilder()
+		.setName('echo')
+		.setDescription('Replies with your input')
+		.addStringOption(option => option.setName('input').setDescription('The input to echo back'))
+		.addChannelOption(option => option.setName('channel').setDescription('The chanel to echo into')),
+	async execute(interaction) {
+		await interaction.reply('Pickle!');
+	},
+};
+
+export default exportedMethods;
