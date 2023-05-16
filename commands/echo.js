38b395df43a36c3475e3df3125daebb89e3f4a08
Parshotan Seenanan <parshsee@gmail.com>
2023-05-15 23:50:57 -0400

    added notes and file for advanced command creation

--- commands/echo.js
+++ commands/echo.js
@@ -4,7 +4,10 @@ const exportedMethods = {
 	data: new SlashCommandBuilder()
 		.setName('echo')
 		.setDescription('Replies with your input')
-		.addStringOption(option => option.setName('input').setDescription('The input to echo back'))
+		.addStringOption(option =>
+			option.setName('input')
+				.setDescription('The input to echo back')
+				.setRequired(true))
 		.addChannelOption(option => option.setName('channel').setDescription('The chanel to echo into')),
 	async execute(interaction) {
 		await interaction.reply('Pickle!');
