be55386f57a9e8be33d8f60dde3379eaaa6551fd
Parshotan Seenanan <parshsee@gmail.com>
2023-05-18 20:55:45 -0400

    added notes and file for using autocomplete

---
+++ commands/guide.js
@@ -0,0 +1,41 @@
+import { SlashCommandBuilder } from 'discord.js';
+
+const exportedMethods = {
+	data: new SlashCommandBuilder()
+		.setName('guide')
+		.setDescription('Search discordjs.guide!')
+		.addStringOption(option =>
+			option
+				.setName('query')
+				.setDescription('Phrase to search for')
+				.setAutocomplete(true))
+		.addStringOption(option =>
+			option
+				.setName('version')
+				.setDescription('Version to search in')
+				.setAutocomplete(true)),
+	async automcomplete(interaction) {
+		// handle the automcompletion response
+		const focusedOption = interaction.options.getFocused(true);
+		let choices;
+
+		if (focusedOption.name === 'query') {
+			choices = ['Popular Topics: Threads', 'Sharding: Getting started', 'Library: Voice Connections', 'Interactions: Replying to slash commands', 'Popular Topics: Embed preview'];
+		}
+
+		if (focusedOption.name === 'version') {
+			choices = ['v9', 'v11', 'v12', 'v13', 'v14'];
+		}
+
+		const filtered = choices.filter(choice => choice.startsWith(focusedOption.value));
+		await interaction.respond(
+			filtered.map(choice => ({ name: choice, value: choice })),
+		);
+	},
+	async execute(interaction) {
+		// respond to the complete slash command
+		await interaction.reply('Poop');
+	},
+};
+
+export default exportedMethods;
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -6,28 +6,46 @@ import { Events } from 'discord.js';
 const exportedMethods = {
 	name: Events.InteractionCreate,
 	async execute(interaction) {
-		if (!interaction.isChatInputCommand()) return;
+		if (interaction.isChatInputCommand()) {
 
-		// Get the matching command from client.commands Collection based on the interaction.name
-		// client is always available vis interaction.client
-		const command = interaction.client.commands.get(interaction.commandName);
+			// Get the matching command from client.commands Collection based on the interaction.name
+			// client is always available vis interaction.client
+			const command = interaction.client.commands.get(interaction.commandName);
 
-		// If no matching command is found, log an error and ignore the event
-		if (!command) {
-			console.error(`No command matching ${interaction.commandName} was found.`);
-			return;
-		}
+			// If no matching command is found, log an error and ignore the event
+			if (!command) {
+				console.error(`No command matching ${interaction.commandName} was found.`);
+				return;
+			}
 
-		// Try to execute the command
-		// Catch and log any error the console
-		try {
-			await command.execute(interaction);
-		} catch (error) {
-			console.error(error);
-			if (interaction.replied || interaction.deferred) {
-				await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-			} else {
-				await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+			// Try to execute the command
+			// Catch and log any error the console
+			try {
+				await command.execute(interaction);
+			} catch (error) {
+				console.error(error);
+				if (interaction.replied || interaction.deferred) {
+					await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+				} else {
+					await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+				}
+			}
+		} else if (interaction.isAutocompelete()) {
+			const command = interaction.client.commands.get(interaction.commandName);
+
+			if (!command) {
+				console.error(`No command matching ${interaction.commandName} was found.`);
+			}
+
+			try {
+				await command.autocomplete(interaction);
+			} catch (error) {
+				console.error(error);
+				if (interaction.replied || interaction.deferred) {
+					await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+				} else {
+					await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+				}
 			}
 		}
 	},
