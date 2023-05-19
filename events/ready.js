5015a7e7a0b71909e305dab75bc97f6bf3caaf4a
Parshotan Seenanan <parshsee@gmail.com>
2023-05-18 21:45:11 -0400

    set bot username and activity

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
-import { Events } from 'discord.js';
+import { Events, ActivityType } from 'discord.js';
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
@@ -6,6 +6,9 @@ const exportedMethods = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
+		client.user.setUsername('ImmatureBot V2');
+		client.user.setActivity('you', { type: ActivityType.Watching });
+
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
