6cb93f95abc3408a82c63af5dff002f11d50ff40
Parshotan Seenanan <parshsee@gmail.com>
2023-05-20 21:14:16 -0400

    added notes and files for using buttons

--- commands/ban.js
+++ commands/ban.js
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
+import { SlashCommandBuilder, PermissionFlagsBits, ButtonBuilder, ButtonStyle, ActionRowBuilder } from 'discord.js';
 
 const exportedMethods = {
 	data: new SlashCommandBuilder()
@@ -22,7 +22,42 @@ const exportedMethods = {
 		// Since it's not required, have nullish coalescing operator to provide a value if it's not given
 		const reason = interaction.options.getString('reason') ?? 'No reason provided';
 
-		await interaction.reply(`Banning ${target.username} for reason: ${reason}`);
+		// Creating a confirm button
+		const confirm = new ButtonBuilder()
+			.setCustomId('confirm')
+			.setLabel('Confirm Ban')
+			.setStyle(ButtonStyle.Danger);
+
+		// Creating a cancel button
+		const cancel = new ButtonBuilder()
+			.setCustomId('cancel')
+			.setLabel('Cancel')
+			.setStyle(ButtonStyle.Secondary);
+
+		// Creating a link button
+		const linkExample = new ButtonBuilder()
+			.setLabel('discord.js docs')
+			.setURL('https://discord.js.org')
+			.setStyle(ButtonStyle.Link);
+
+		// Creating a disabled button
+		const disabledExample = new ButtonBuilder()
+			.setCustomId('disabled')
+			.setLabel('I am disabled')
+			.setStyle(ButtonStyle.Primary)
+			.setDisabled(true);
+
+		// Creating an action row
+		// The order you add components is the order they will show up
+		const row = new ActionRowBuilder()
+			.addComponents(cancel, confirm, linkExample, disabledExample);
+
+		// Replying with a message and components
+		// Becomes an object, with content and components
+		await interaction.reply({
+			content: `Banning ${target.username} for reason: ${reason}`,
+			components: [row],
+		});
 		await interaction.guild.members.ban(target);
 	},
 };
