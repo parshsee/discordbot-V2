722ee76b41f70eedd5a693a2a41c57fcdce0057b
Parshotan Seenanan <parshsee@gmail.com>
2023-05-29 18:21:31 -0400

    added schedule on ready to check every minute that twitch token is valid

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,13 @@
 import { Events, ActivityType } from 'discord.js';
+import { scheduleJob } from 'node-schedule';
+import * as helper from '../utils/helper.js';
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 const exportedMethods = {
 	name: Events.ClientReady,
 	once: true,
-	execute(client) {
+	async execute(client) {
 		client.user.setUsername('ImmatureBot V2');
 		client.user.setActivity('you', { type: ActivityType.Watching });
 
@@ -18,6 +20,10 @@ const exportedMethods = {
 			Create job schedules for all necessary DB functions once implemented
 				- Birthday, Event(?), Twitch Token, Streamer
 		*/
+
+		// Use node-scheduler to create a cron-job that runs every minute
+		// Calls the twitch token validator, to check that the token is not expired (if it is, it will retrieve a new token as well)
+		scheduleJob('*/1 * * * *', async () => helper.twitchTokenValidator());
 
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
