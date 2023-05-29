52c824338ec89ca4197b46d891fee38764009fcd
Parshotan Seenanan <parshsee@gmail.com>
2023-05-29 18:20:46 -0400

    added api calls to get twitch token and validate twitch token not expired

--- utils/helper.js
+++ utils/helper.js
@@ -53,6 +53,45 @@ const memeCreationAPI = async (image, topText, bottomText) => {
 	}
 };
 
+const gameAPI = async (gameName, gameYear) => {
+
+};
+
+/*	Twitch Token has an expiration timer
+	This function calls the token validation API, passing in the existing token in the header
+	Checks whether the token has expired or not
+	https://dev.twitch.tv/docs/authentication/validate-tokens/
+*/
+const twitchTokenValidator = async () => {
+	try {
+		// Axios call to check if token is valid
+		// Destructure the data into twitchValidator const
+		// Pass the current token in headers
+		const { data: twitchValidator } = await axios({
+			url: process.env.TWITCH_TOKEN_VALIDATION_API,
+			method: 'GET',
+			headers: {
+				Authorization: `OAuth ${process.env.TWITCH_TOKEN}`,
+			},
+		});
+
+		// Check the response object for expires_in key
+		// It represents the seconds left until the token expires
+		if (twitchValidator.expires_in > 0) {
+			console.log(`Twitch Token Time Remaining: ${twitchValidator.expires_in}`);
+		}
+	} catch (error) {
+		console.log('Call to Twitch Validator API Failed:', error.response.data);
+		// If the API call throws, check if it contains a status code and message
+		// A status of 401 and appropriate message indicates the token expired
+		if (error.response.data.status === 401 && error.response.data.message === 'invalid access token') {
+			console.log('Token Expired, Retrieving New Token');
+			// If the token is expired, call the function to get new token
+			await getTwitchToken();
+		}
+	}
+};
+
 // =============================== Helper Functions ===============================
 
 const filterText = (text) => {
@@ -71,8 +110,38 @@ const filterText = (text) => {
 	return filteredText;
 };
 
+/*	This function retrieves and sets the Twitch Token into the .env file
+	https://dev.twitch.tv/docs/authentication/
+*/
+const getTwitchToken = async () => {
+	try {
+		// Axios call to get twitch token
+		// Destructure data to twitchInfo const
+		// Post request to endpoint, with appropriate params
+		//	Check Twitch Doc to see exactly what's used
+		const { data: twitchInfo } = await axios({
+			url: process.env.TWITCH_TOKEN_API,
+			method: 'POST',
+			params: {
+				'client_id': process.env.TWITCH_CLIENT_ID,
+				'client_secret': process.env.TWITCH_CLIENT_SECRET,
+				'grant_type': 'client_credentials',
+			},
+		});
+
+		// If the call was successful, log the old token
+		console.log(`Old Twitch Token: ${process.env.TWITCH_TOKEN}`);
+		// Set the token in the .env file to the newly retrieved token
+		process.env.TWITCH_TOKEN = twitchInfo.access_token;
+	} catch (error) {
+		console.error('Call to Twitch Token API Failed:', error);
+	}
+};
+
 export {
 	dailyCuteAPI,
 	memeAPI,
 	memeCreationAPI,
+	gameAPI,
+	twitchTokenValidator,
 };
