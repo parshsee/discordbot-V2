0c021238eb9ff8563af348c49eb61ff5afbf7471
Parshotan Seenanan <parshsee@gmail.com>
2023-05-31 20:09:15 -0400

    added most functions for logging when member joins or leaves a guild

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,22 +1,48 @@
-import { Events } from 'discord.js';
+import { EmbedBuilder, Events } from 'discord.js';
 
 const exportedMethods = {
 	name: Events.GuildMemberRemove,
-	async execute(client) {
-		console.log(client);
+	async execute(guildMember) {
+		const guild = guildMember.guild;
 
-		/* TODO:
-			Check that the 'member-log' channel exists in the guild
-			If it does
-				get that channels id
-				Create an embed that the user has left
-				Send the embed to that channel
-			If it doesn't
-				Log that channel doesn't exist
-				Send message to default(original/first) channel saying action couldn't be performed ???
-				Have command /create-required-channels that will check and create any needed channels if they don't exist ???
-					Make it admin/moderator level only ???
-		*/
+
+		// Check if the guild is available to send message to
+		if (!guild.available) {
+			console.log('Error: Guild not available for member logging (remove)');
+			return;
+		}
+
+		// Check that member log channel exists within guild
+		// Will return the channel info or undefined
+		const memberLogChannel = guild.channels.cache.find(channel => channel.name === 'member-log');
+
+		// If there is no member-log channel
+		if (!memberLogChannel) {
+			// Log that the guild doesn't have a member-log channel
+			console.log(`Error: ${guild.name} Guild does not have member-log channel for logging (remove)`);
+
+			// TODO:
+			// Send message to default(original/ first) channel saying action couldn't be performed ???
+			// Have command / create - required - channels that will check and create any needed channels if they don't exist ???
+			// Make it admin / moderator level only ???
+
+			// Exit out of function
+			return;
+		}
+
+		// Create and send an embed that the user has joined
+		const embed = new EmbedBuilder()
+			.setFooter({
+				text: `${guildMember.user.tag} has left.`,
+				iconURL: guildMember.user.displayAvatarURL(),
+			});
+
+
+		console.log(`${guildMember.user.tag} has left ${guild.name}`);
+
+		// Send the embed in the member-log channel
+		memberLogChannel.send({ embeds: [embed] });
+
 	},
 };
 
