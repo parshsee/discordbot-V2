8c16c04a8c0f77744a5cc7cea45a43b698ea5490
Parshotan Seenanan <parshsee@gmail.com>
2023-06-01 22:15:55 -0400

    added error handling for if member-log channel not available

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,4 +1,4 @@
-import { EmbedBuilder, Events } from 'discord.js';
+import { EmbedBuilder, Events, PermissionFlagsBits } from 'discord.js';
 
 const exportedMethods = {
 	name: Events.GuildMemberRemove,
@@ -21,10 +21,27 @@ const exportedMethods = {
 			// Log that the guild doesn't have a member-log channel
 			console.log(`Error: ${guild.name} Guild does not have member-log channel for logging (remove)`);
 
-			// TODO:
-			// Send message to default(original/ first) channel saying action couldn't be performed ???
-			// Have command / create - required - channels that will check and create any needed channels if they don't exist ???
-			// Make it admin / moderator level only ???
+			// Get the system channel (channel where default messages go to)
+			const sysChannel = guild.systemChannel;
+
+			// If the system channel is null (deleted/disabled/removed from guild)
+			if (!sysChannel) {
+				// Find another channel to send error message to
+				//		- Check that the channel is type 0 --- TextChannel type
+				//		- Check that the permissions for the bot (using bots id) for the channel includes SendMessages (check that the bot can send messages to this text channel)
+				const backupChannel = guild.channels.cache.find(channel => channel.type === 0 && channel.permissionsFor(guild.client.user.id).any(PermissionFlagsBits.SendMessages));
+
+				// If there are no text channels that the bot can send messages to, log an additional error message and exit
+				if (!backupChannel) {
+					console.log(`Error: ${guild.name} Guild has no text channels that bot can send messages to. member-logging (remove) not performed`);
+					return;
+				}
+				// Send error message to backup channel that required channels are not available
+				return backupChannel.send({ content: `Error: ${guild.name} does not have a member-log channel for logging (remove).\nA moderator can run the slash command '/create-required-channels' to create the necessary channels` });
+			}
+
+			// Send error message to backup channel that required channels are not available
+			sysChannel.send({ content: `Error: ${guild.name} does not have a member-log channel for logging (remove).\nA moderator can run the slash command '/create-required-channels' to create the necessary channels` });
 
 			// Exit out of function
 			return;
