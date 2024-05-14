3a374b0a8ad317970da6771804dec5dff577ac7e
Parshotan Seenanan <parshsee@gmail.com>
2024-05-14 18:17:04 -0400

    Changed schema to store Discord ID instead of names

--- data/models/quotes.js
+++ data/models/quotes.js
@@ -10,8 +10,7 @@ import mongoose from 'mongoose';
 // 	for the quotes schema
 const quotesSchema = mongoose.Schema({
 	id: { type: Number, required: true, unique: true },
-	fName: { type: String, required: true, trim: true },
-	lName: { type: String, required: true, trim: true },
+	userId: { type: Number, required: true, trim: true },
 	quote: { type: String, required: true, trim: true },
 	timestamp: { type: Date, required: true, default: Date.now },
 }, { autoIndex: false, autoCreate: false });
