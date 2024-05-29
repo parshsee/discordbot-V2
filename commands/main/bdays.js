1674c2159bfba3e3d9888b1ab65eeefde2021beb
Parshotan Seenanan <parshsee@gmail.com>
2024-05-28 22:35:27 -0400

    Added code to sort birthdays by upcoming and list them

--- commands/main/bdays.js
+++ commands/main/bdays.js
@@ -56,6 +56,11 @@ const exportedMethods = {
 				// Get the guild document from the database
 				const guild = await Guilds.findById({ _id: guildId });
 
+				// TODO
+				// Check if fName and lName already exist within guilds birthdays subdoc
+				// SHOULD NOT BE ABLE TO ADD SAME USER MULTIPLE TIMES
+				// ********************************************************************
+
 				console.log(`Guild DB called for ${interaction.guild.name}: Bdays - Add`);
 
 				const birthday = new Birthdays({
@@ -161,7 +166,83 @@ const exportedMethods = {
 			}
 
 		} else if (interaction.options.getSubcommand() === 'list') {
+			// Get the guild id
+			const guildId = interaction.guild.id;
+			// Create reply message
+			let reply = '';
 
+			try {
+				// Destructure the birthdays subdoc array from the guild doc in DB
+				const { birthdays } = await Guilds.findById({ _id: guildId });
+				console.log(birthdays);
+
+				console.log(`Guild DB called for ${interaction.guild.name}: Bdays - List`);
+
+				// Check that the birthdays array is empty (no birthdays in DB)
+				if (!birthdays.length) {
+					await interaction.editReply({
+						content: 'No birthdays exist in database!',
+					});
+					return;
+				}
+
+				// Get current date
+				const currentDate = new Date();
+				currentDate.setHours(0, 0, 0, 0);
+				// Initalize arrays for dates before and after current day
+				const afterCurrDate = [];
+				const beforeCurrDate = [];
+
+				birthdays.forEach(birthday => {
+					// Check if the month is ahead
+					if (birthday.birthday.getMonth() + 1 > currentDate.getMonth() + 1) {
+						// Add to array of dates AFTER current day
+						afterCurrDate.push({
+							bday: birthday.birthday,
+							fullName: `${birthday.fName} ${birthday.lName}`,
+						});
+						// If the month is the same, check if the date is ahead
+					} else if (birthday.birthday.getMonth() + 1 === currentDate.getMonth() + 1 && birthday.birthday.getDate() > currentDate.getDate()) {
+						// Add to array of dates AFTER current day
+						afterCurrDate.push({
+							bday: birthday.birthday,
+							fullName: `${birthday.fName} ${birthday.lName}`,
+						});
+						// Else the month/day is in the past
+					} else {
+						// Add to array of dates BEFORE current day
+						beforeCurrDate.push({
+							bday: birthday.birthday,
+							fullName: `${birthday.fName} ${birthday.lName}`,
+						});
+					}
+				});
+
+				// Get the sorted arrays
+				const sortedAfterDates = sortArr(afterCurrDate, currentDate);
+				// For each object in the array, format the date and add to reply
+				sortedAfterDates.forEach(birthday => {
+					// Format the date to mm/dd/yyyy
+					const date = `${birthday.bday.toLocaleDateString()}`;
+					// Add to the reply
+					reply += `:birthday: **${birthday.fullName}** \n Birthday: ${date} \n\n`;
+				});
+
+				// Get the sorted arrays
+				const sortedBeforeDates = sortArr(beforeCurrDate, currentDate);
+				// For each object in the array, format the date and add to reply
+				sortedBeforeDates.forEach(birthday => {
+					// Format the date to mm/dd/yyyy
+					const date = `${birthday.bday.toLocaleDateString()}`;
+					// Add to the reply
+					reply += `:birthday: **${birthday.fullName}** \n Birthday: ${date} \n\n`;
+				});
+
+				return createAndSendEmbed(reply, interaction);
+
+			} catch (error) {
+				console.log(error);
+			}
 		}
 
 		// TODO: Delete
@@ -200,6 +281,72 @@ const validateBirthdate = (userBirthdate, birthdateArray) => {
 	}
 
 	return date;
+};
+
+const sortArr = (arr, currentDate) => {
+	// Get the current month
+	const currMonth = currentDate.getMonth() + 1;
+
+	// Sort the array using sort method
+	// Returning -1 to sort, sorts a to an index lower than b (i.e. a comes first)
+	// Returning 0 to sort, leave a and b unchanged with respect to each other, but sorted with respect to all different elements
+	// Returning 1 to sort, sorts b to an index lower than a (i.e. b comes first)
+	arr.sort((a, b) => {
+		// Get the birthday month and day of the SECOND value (idk why is does second value as a)
+		const bdayMonth = a.bday.getMonth() + 1;
+		const bdayDay = a.bday.getDate();
+
+		// Get the birthday month and day of the FIRST value
+		const bdayMonth2 = b.bday.getMonth() + 1;
+		const bdayDay2 = b.bday.getDate();
+
+		// If the second month is less than the first month swap them
+		if (bdayMonth - currMonth < bdayMonth2 - currMonth) {
+			return -1;
+		} else if (bdayMonth - currMonth === bdayMonth2 - currMonth && bdayDay < bdayDay2) {
+			return -1;
+		}
+	});
+	return arr;
+};
+
+/*
+  Take a string and the interaction and sends 'x'  amount of messages to the channel depending
+  on the length of the text.
+  Async Function to allow for loop to create a message, send the message, then create another
+  if the text was long enough to be broken into multiple arrays and send that
+  ([a]wait for embed to be made before sending it)
+  Taken from freestuff code
+*/
+const createAndSendEmbed = async (text, interaction) => {
+	const testArr = helper.chunkSubstr(text, 2048);
+	// Create generic Embedded Message with necessary fields
+	const embed = helper.createIntitialEmbed(interaction.client);
+
+	let count = 1;
+
+	// Loop through every element
+	for (const chunk of testArr) {
+		// First Embedded Title this
+		// Else every other embedded gets 'Games Cont'
+		if (count === 1) {
+			embed
+				.setTitle('Birthdays');
+			// .setThumbnail(message.guild.iconURL());
+		} else {
+			embed
+				.setTitle('Birthdays Cont.');
+			// .setThumbnail();
+		}
+		embed
+			.setDescription(chunk);
+		count += 1;
+
+		// Send a followUp interaction with the embed chunk
+		await interaction.followUp({
+			embeds: [embed],
+		});
+	}
 };
 
 export default exportedMethods;
